de508a25ee6d0bd3028c0c80a860957c2e080d96
Jef van Schendel <mail@jefvanschendel.nl>
2012-12-29 19:29:40 +0100

    update

--- bin/generate-magicmap.py
+++
@@ -1,4 +0,0 @@
-from jinja2 import Environment, PackageLoader
-env = Environment(loader=PackageLoader('yourapplication', 'templates'))
-template = env.get_template('mytemplate.html')
-print template.render(the='variables', go='here')
--- generate-magicmap.py
+++
@@ -1,6 +0,0 @@
-from jinja2 import Environment, PackageLoader
-env = Environment(loader=PackageLoader('magicmap', 'templates'))
-template = env.get_template('template.html')
-output = template.render(the='variables', go='here')
-with open("template.html", "w") as f:
-    f.write(output)
